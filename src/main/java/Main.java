a2708123ff6293f845c8c582f582ed226c1cc68b
Hiren <galahiren3@gmail.com>
2025-04-29 16:09:35 +0530

    First Commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World on ");
+        System.out.println("Hello World I will complete this by this week ");
     }
 }
